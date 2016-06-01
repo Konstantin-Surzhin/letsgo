72baf92735606f98cd7d8a1c8f8777f95b3ee512
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-06-01 15:29:11 +0300

    I have added Mockito interface

---
+++ letsgo-web/src/test/java/igo/user/Mockito/SignUpTest.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2016 surzhin.konstantin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package igo.user.Mockito;
+
+import igo.user.SignUp;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.igo.UserManagerBeanRemote;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author surzhin.konstantin
+ */
+public class SignUpTest {
+
+    public SignUpTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of processRequest method, of class SignUp. User manager Bean is
+     * null.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testProcessRequestUMBean1() throws Exception {
+        System.out.println("user manager Bean is null");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        try (PrintWriter writer = new PrintWriter(out)) {
+
+            String name = "user";
+            String passwd = "passwd";
+            String rpasswd = passwd;
+
+            when(request.getParameter("name")).thenReturn(name);
+            when(request.getParameter("passwd")).thenReturn(passwd);
+            when(request.getParameter("rpasswd")).thenReturn(rpasswd);
+
+            when(response.getWriter()).thenReturn(writer);
+
+            SignUp instance = new SignUp();
+
+            instance.processRequest(request, response);
+        }
+        assertEquals("user manager is null", out.toString());
+    }
+
+    /**
+     * Test of processRequest method, of class SignUp. User manager Bean is not
+     * null.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testProcessRequestUMBean2() throws Exception {
+        System.out.println("user manager Bean is not null");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+
+            String name = "user";
+            String passwd = "passwd";
+            String rpasswd = passwd;
+
+            when(request.getParameter("name")).thenReturn(name);
+            when(request.getParameter("passwd")).thenReturn(passwd);
+            when(request.getParameter("rpasswd")).thenReturn(rpasswd);
+
+            when(response.getWriter()).thenReturn(writer);
+
+            SignUp instance = new SignUp();
+
+            UserManagerBeanRemote userManagerBean = mock(UserManagerBeanRemote.class);
+            instance.setUserManagerBean(userManagerBean);
+            
+            instance.processRequest(request, response);
+
+            verify(userManagerBean, times(1)).create(name, passwd, rpasswd);
+        }
+
+        assertTrue(out.toString().contains("User id: "));
+    }
+
+    @Test
+    public void testProcessRequestName1() throws Exception {
+        System.out.println("name is null");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+        assertEquals("name is null", out.toString());
+    }
+
+    @Test
+    public void testProcessRequestName2() throws Exception {
+        System.out.println("name is empty");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            when(request.getParameter("name")).thenReturn("");
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+
+        assertEquals("name is empty", out.toString());
+    }
+
+    @Test
+    public void testProcessRequestPasswd1() throws Exception {
+        System.out.println("passwd is null");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            when(request.getParameter("name")).thenReturn("user");
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+        assertEquals("passwd is null", out.toString());
+    }
+
+    @Test
+    public void testProcessRequestPasswd2() throws Exception {
+        System.out.println("passwd is empty");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            when(request.getParameter("name")).thenReturn("user");
+            when(request.getParameter("passwd")).thenReturn("");
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+
+        assertEquals("passwd is empty", out.toString());
+    }
+
+    @Test
+    public void testProcessRequestRPasswd1() throws Exception {
+        System.out.println("rpasswd is null");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            when(request.getParameter("name")).thenReturn("user");
+            when(request.getParameter("passwd")).thenReturn("passwd");
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+        assertEquals("rpasswd is null", out.toString());
+    }
+
+    @Test
+    public void testProcessRequestRPasswd2() throws Exception {
+        System.out.println("rpasswd is empty");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            when(request.getParameter("name")).thenReturn("user");
+            when(request.getParameter("passwd")).thenReturn("passwd");
+            when(request.getParameter("rpasswd")).thenReturn("");
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+
+        assertEquals("rpasswd is empty", out.toString());
+    }
+
+    @Test
+    public void testProcessRequestPasswdRPasswdNotEquals() throws Exception {
+        System.out.println("passwd and rpasswd not equals");
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter writer = new PrintWriter(out)) {
+            when(response.getWriter()).thenReturn(writer);
+            when(request.getParameter("name")).thenReturn("user");
+            when(request.getParameter("passwd")).thenReturn("passwd");
+            when(request.getParameter("rpasswd")).thenReturn("rpasswd");
+            SignUp instance = new SignUp();
+            instance.processRequest(request, response);
+        }
+
+        assertEquals("A password mismatch has been detected", out.toString());
+    }
+}
---
+++ letsgo-web/src/test/java/igo/user/Mockito/UserTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2016 surzhin.konstantin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package igo.user.Mockito;
+
+import igo.user.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author surzhin.konstantin
+ */
+public class UserTest {
+    
+    /**
+     *
+     */
+    public UserTest() {
+    }
+    
+    /**
+     *
+     */
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    /**
+     *
+     */
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    /**
+     *
+     */
+    @Before
+    public void setUp() {
+    }
+    
+    /**
+     *
+     */
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class User.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        User instance = new User();
+        Long expResult = 1L;
+        instance.setId(expResult);
+        
+        Long result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setId method, of class User.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        Long id = null;
+        User instance = new User();
+        instance.setId(id);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class User.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        User instance = new User();
+        String expResult = "Tom";
+        instance.setName(expResult);
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setName method, of class User.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "";
+        User instance = new User();
+        instance.setName(name);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNikname method, of class User.
+     */
+    @Test
+    public void testGetNikname() {
+        System.out.println("getNikname");
+        User instance = new User();
+        String  expResult= "Don";
+        instance.setNickName(expResult);
+        String result = instance.getNickName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNikname method, of class User.
+     */
+    @Test
+    public void testSetNikname() {
+        System.out.println("setNikname");
+        String nikname = "";
+        User instance = new User();
+        instance.setNickName(nikname);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
