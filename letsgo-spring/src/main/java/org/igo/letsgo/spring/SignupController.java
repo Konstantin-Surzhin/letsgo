9d5ae36c64e72f4f883479e48d00e328ab5fea41
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-06-03 11:27:40 +0300

    I have added spring index and signup pages

---
+++ letsgo-spring/src/main/java/org/igo/letsgo/spring/SignupController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.igo.letsgo.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author surzhin.konstantin
+ */
+@Controller
+public class SignupController {
+
+    @RequestMapping(value = "/signup")
+    public ModelAndView sayHello() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Let's signup!");
+        mv.setViewName("signup");
+        return mv;
+    }
+}
---
+++ letsgo-spring/src/main/webapp/WEB-INF/jsp/signup.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : signup
+    Created on : 03.06.2016, 9:51:04
+    Author     : surzhin.konstantin
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>SignUp Page</title>
+    </head>
+    
+    <body>
+        <h1>${message}</h1>
+        
+    </body>
+</html>
