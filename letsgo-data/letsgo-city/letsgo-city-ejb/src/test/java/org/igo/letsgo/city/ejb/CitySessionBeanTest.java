ada89524811866d3bf12416c72b8503da9808dbe
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-08-24 16:01:58 +0300

    I have added ejb tests prototypes

--- letsgo-data/letsgo-city/letsgo-city-ejb/src/test/java/org/igo/letsgo/city/ejb/CitySessionBeanTest.java
+++ letsgo-data/letsgo-city/letsgo-city-ejb/src/test/java/org/igo/letsgo/city/ejb/CitySessionBeanTest.java
@@ -5,7 +5,11 @@
  */
 package org.igo.letsgo.city.ejb;
 
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
 import org.igo.letsgo.remote.ICityRemote;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,40 +23,48 @@ import static org.junit.Assert.*;
  * @author surzhin.konstantin
  */
 public class CitySessionBeanTest {
-    
+
+    private EJBContainer container;
+
     public CitySessionBeanTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        Properties properties = new Properties();
+        properties.setProperty(EJBContainer.MODULES, "letsgo-city-ejb");
+        properties.put(EJBContainer.PROVIDER, "tomee-embedded");
+        container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
     }
-    
+
     @After
     public void tearDown() {
+        container.close();
     }
 
     /**
      * Test of getCity method, of class CitySessionBean.
-     * @throws java.lang.Exception
+     *
+     *
      */
     @Test
-    public void testGetCity() throws Exception {
+    public void testGetCity() {
         System.out.println("getCity");
-        int id = 0;
-        try (EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer()) {
-            ICityRemote instance = (ICityRemote)container.getContext().lookup("java:global/classes/CitySessionBean");
+        try {
+            ICityRemote instance = (ICityRemote) container.getContext().lookup("java:global/letsgo-city-ejb/CitySessionBean!org.igo.letsgo.remote.ICityRemote");
             String expResult = "Moskow";
-            String result = instance.getCity(id);
+            String result = instance.getCity(0);
             assertEquals(expResult, result);
+        } catch (NamingException ex) {
+            System.out.println(ex.getExplanation());
         }
     }
-    
 }
