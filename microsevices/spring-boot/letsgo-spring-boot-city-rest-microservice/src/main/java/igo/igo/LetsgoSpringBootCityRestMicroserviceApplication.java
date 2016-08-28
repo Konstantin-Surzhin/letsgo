cff14ef038ea036f75306cb3b2cd9b80b61505f3
pl <pl@localhost>
2016-08-28 12:36:30 +0300

    I have added spring boot city microservice

---
+++ microsevices/spring-boot/letsgo-spring-boot-city-rest-microservice/src/main/java/igo/igo/LetsgoSpringBootCityRestMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package igo.igo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsgoSpringBootCityRestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsgoSpringBootCityRestMicroserviceApplication.class, args);
+	}
+}
