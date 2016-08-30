70ec886ae34756e8bc890bb21e278bb8976e7eea
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-08-30 15:46:53 +0300

    I have added spring freemarker city project

---
+++ letsgo-spring/spring-freemarker/letsgo-spring-freemarker-city/src/main/java/org/igo/letsgo/spring/freemarker/city/CityController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.igo.letsgo.spring.freemarker.city;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author surzhin.konstantin
+ */
+@Controller
+public class CityController {
+
+    @RequestMapping(value = "/index")
+    public ModelAndView sayHello() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Hello World!");
+        mv.setViewName("index");
+        return mv;
+    }
+}
