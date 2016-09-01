2bf9eca5f8719c15d57067c8bc29270393a0fe40
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-09-01 15:28:44 +0300

    I have added buisness methods for city entity.

--- letsgo-data/letsgo-city/letsgo-city-ejb/src/main/java/org/igo/letsgo/city/ejb/CitySessionBean.java
+++ letsgo-data/letsgo-city/letsgo-city-ejb/src/main/java/org/igo/letsgo/city/ejb/CitySessionBean.java
@@ -4,11 +4,16 @@
  */
 package org.igo.letsgo.city.ejb;
 
+import com.sun.istack.internal.logging.Logger;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import org.igo.entities.City;
+import org.igo.entities.User;
 import org.igo.letsgo.remote.ICityRemote;
 
 /**
@@ -18,7 +23,7 @@ import org.igo.letsgo.remote.ICityRemote;
 @Stateless
 public class CitySessionBean implements ICityRemote {
 
-    
+    Logger logger = Logger.getLogger(CitySessionBean.class);
     private EntityManager entityManager;
 
     @Override
@@ -29,22 +34,20 @@ public class CitySessionBean implements ICityRemote {
 
     @Override
     public Integer getCityId(final String name) {
-        Integer id = null;
+
         TypedQuery<City> q = getEntityManager().createNamedQuery("City.findByCityName", City.class);
         q.setParameter("cityName", name);
 
         try {
-            id = q.getSingleResult().getId();
+            return q.getSingleResult().getId();
         } catch (Exception ex) {
-            ex.getLocalizedMessage();
+            return -1; //id not found
         }
-        return id;
     }
 
     /**
      * @return the em
      */
-    
     public EntityManager getEntityManager() {
         return entityManager;
     }
@@ -56,6 +59,48 @@ public class CitySessionBean implements ICityRemote {
     public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    
-    
+
+    @Override
+    public List<String> getAllCityName(String name) {
+        TypedQuery<City> q = getEntityManager().createNamedQuery("City.findAll", City.class);
+        List<City> names = q.getResultList();
+
+        List<String> listCityName = names.stream()
+                .map(c -> c.getCityName())
+                .sorted()
+                .collect(Collectors.toList());
+        return listCityName;
+    }
+
+    @Override
+    public List<String> getAllCityUser(final String name) {
+
+        TypedQuery<City> q = getEntityManager().createNamedQuery("City.findByCityName", City.class);
+        q.setParameter("cityName", name);
+
+        try {
+            Integer id = q.getSingleResult().getId();
+            City city = getEntityManager().find(City.class, id);
+            Set<User> users = city.getUserCollection();
+            List<String> listUsersName = users.stream()
+                    .map(c -> c.getName())
+                    .sorted()
+                    .collect(Collectors.toList());
+            return listUsersName;
+        } catch (Exception ex) {
+            ex.getLocalizedMessage();
+            return null;
+        }
+    }
+
+    @Override
+    public List<String> getAllCityUser(final Integer id) {
+        City city = getEntityManager().find(City.class, id);
+        Set<User> users = city.getUserCollection();
+        List<String> listUsersName = users.stream()
+                .map(c -> c.getName())
+                .sorted()
+                .collect(Collectors.toList());
+        return listUsersName;
+    }
 }
