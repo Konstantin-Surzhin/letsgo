146ae427c575fced713ac48c4fec0220b0e4ebbc
pl <pl@localhost>
2016-09-04 14:51:57 +0300

    I have added gpl3 licence headers

--- letsgo-data/letsgo-entity/src/test/java/org/igo/junit/entities/LetsgoEntityTestSuite.java
+++ letsgo-data/letsgo-entity/src/test/java/org/igo/junit/entities/LetsgoEntityTestSuite.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package java.org.igo.entities;
+package org.igo.junit.entities;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
