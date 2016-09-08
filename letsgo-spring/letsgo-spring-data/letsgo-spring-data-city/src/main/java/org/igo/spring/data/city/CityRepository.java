dca49da29cc73cd10e85c6b894e310aac9cdd552
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-09-08 11:49:23 +0300

    I have added spring-data city projects.

---
+++ letsgo-spring/letsgo-spring-data/letsgo-spring-data-city/src/main/java/org/igo/spring/data/city/CityRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.igo.spring.data.city;
+
+import java.util.List;
+import org.igo.entities.City;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author surzhin.konstantin
+ */
+public interface CityRepository extends CrudRepository<City, Long>{
+    
+    City findByFirstName(String firstName);
+    List<City> findByLastName(String lastName);
+}
+
