6739d78adbb5e715b09ea6b315c7f7f1728b888d
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-09-19 17:21:48 +0300

    I have added jaxws-maven-plugin for city ms

--- microsevices/spring-boot/CityMS/src/main/java/org/igo/letsgo/spring/boot/degree/CityApplication.java
+++ microsevices/spring-boot/CityMS/src/main/java/org/igo/letsgo/spring/boot/degree/CityApplication.java
@@ -1,11 +1,15 @@
 package org.igo.letsgo.spring.boot.degree;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.igo.letsgo.spring.boot.domain.City;
 import org.igo.letsgo.spring.boot.repository.CityRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -29,5 +33,14 @@ public class CityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CityApplication.class, args);
+    }
+    @Bean
+    public ServletRegistrationBean dispatcherServlet() {
+        return new ServletRegistrationBean(new CXFServlet(), "/soap-api/*");
+    }
+ 
+    @Bean(name=Bus.DEFAULT_BUS_ID)
+    public SpringBus springBus() {      
+        return new SpringBus();
     }
 }
