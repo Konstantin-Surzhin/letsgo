4fa9da53933df4ef1030add15ec6d8d8dd5b739d
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-09-19 16:24:40 +0300

    I have added data jpa and view for city microservice

---
+++ microsevices/spring-boot/CityMS/src/main/java/org/igo/letsgo/spring/boot/degree/CityApplication.java
@@ -0,0 +1,33 @@
+package org.igo.letsgo.spring.boot.degree;
+
+import org.igo.letsgo.spring.boot.domain.City;
+import org.igo.letsgo.spring.boot.repository.CityRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan("org.igo.letsgo.spring.boot.domain")
+@ComponentScan(basePackages = {"org.igo.letsgo.spring.boot"})
+@EnableJpaRepositories("org.igo.letsgo.spring.boot.repository")
+public class CityApplication {
+
+    @Bean
+    InitializingBean saveData(CityRepository repo) {
+        return () -> {
+            repo.save(new City("Москва"));
+            repo.save(new City("Leningad"));
+            repo.save(new City("Voronez"));
+            repo.save(new City("Pskov"));
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityApplication.class, args);
+    }
+}
---
+++ microsevices/spring-boot/CityMS/src/main/java/org/igo/letsgo/spring/boot/web/CityController.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 surzhin.konstantin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.igo.letsgo.spring.boot.web;
+
+import org.igo.letsgo.spring.boot.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author surzhin.konstantin
+ */
+@Controller
+public class CityController {
+
+    @Autowired
+    CityRepository cityRepository;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("city", cityRepository.findAll());
+        return "index";
+    }
+}
