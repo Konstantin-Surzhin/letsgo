1c2d13e9430b6a0097db2aa4ef904eba28f3c738
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-09-21 10:52:03 +0300

    I have fixed city findByName web parameter

--- microsevices/spring-boot/CityMS/src/main/java/org/igo/letsgo/spring/boot/repository/CityRepository.java
+++ microsevices/spring-boot/CityMS/src/main/java/org/igo/letsgo/spring/boot/repository/CityRepository.java
@@ -18,17 +18,17 @@ package org.igo.letsgo.spring.boot.repository;
 
 import java.util.List;
 import org.igo.letsgo.spring.boot.domain.City;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  *
  * @author surzhin.konstantin
  */
-@RepositoryRestResource(path = "city")
+@RepositoryRestResource(collectionResourceRel = "city", path = "city")
 public interface CityRepository extends JpaRepository<City, Integer> {
 
-    @RestResource(path = "names")
-    List<City> findByCityName(String name);
+    List<City> findByCityName(@Param("cityName") String cityName);
 }
