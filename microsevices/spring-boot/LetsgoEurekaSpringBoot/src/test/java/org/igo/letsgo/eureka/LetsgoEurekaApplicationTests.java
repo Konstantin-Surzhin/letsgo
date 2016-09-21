ff78ac9430f68387a08db8a37497bddd4e15b91c
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-09-21 14:59:08 +0300

    I have added Eureka Spring Boot server

---
+++ microsevices/spring-boot/LetsgoEurekaSpringBoot/src/test/java/org/igo/letsgo/eureka/LetsgoEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.igo.letsgo.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LetsgoEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
