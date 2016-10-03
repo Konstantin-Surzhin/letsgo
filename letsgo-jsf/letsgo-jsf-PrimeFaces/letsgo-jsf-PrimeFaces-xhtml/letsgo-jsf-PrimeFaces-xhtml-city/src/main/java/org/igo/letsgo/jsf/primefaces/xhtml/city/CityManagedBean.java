67d62188acbac04e4078a8e2b4c6416965977052
pl <pl@pl-Lenovo-Z580>
2016-10-04 00:05:27 +0300

    I have added primeface bean for city

---
+++ letsgo-jsf/letsgo-jsf-PrimeFaces/letsgo-jsf-PrimeFaces-xhtml/letsgo-jsf-PrimeFaces-xhtml-city/src/main/java/org/igo/letsgo/jsf/primefaces/xhtml/city/CityManagedBean.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2016 pl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.igo.letsgo.jsf.primefaces.xhtml.city;
+
+import java.io.Serializable;
+import java.util.Random;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author pl
+ */
+@ManagedBean(name="CityManagedBean")
+@SessionScoped
+public class CityManagedBean implements Serializable {
+
+    private final Integer randomInt;
+    private String cityName="Житомир";
+    private String pageHeader="Управление городами";
+
+    /**
+     * Creates a new instance of CityManagedBean
+     */
+    public CityManagedBean() {
+                Random randomGR = new Random();
+        randomInt = randomGR.nextInt(10);
+        System.out.println("Duke's number: " + randomInt);
+    }
+
+    /**
+     * @return the cityName
+     */
+    public String getCityName() {
+        return cityName;
+    }
+
+    /**
+     * @param cityName the cityName to set
+     */
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    /**
+     * @return the pageHeader
+     */
+    public String getPageHeader() {
+        return pageHeader;
+    }
+
+    /**
+     * @param pageHeader the pageHeader to set
+     */
+    public void setPageHeader(String pageHeader) {
+        this.pageHeader = pageHeader;
+    }
+    
+}
