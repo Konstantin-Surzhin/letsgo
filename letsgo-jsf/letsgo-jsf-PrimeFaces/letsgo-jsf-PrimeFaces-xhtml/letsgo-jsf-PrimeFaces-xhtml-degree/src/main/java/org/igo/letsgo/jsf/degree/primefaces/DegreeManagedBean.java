88297456073ab6b34c9a4fe6993d574e3cd68e62
pl <pl@pl-Lenovo-Z580>
2016-10-08 14:07:09 +0300

    I have added content to west menu of degree manager PimeFaces index.xhtml

---
+++ letsgo-jsf/letsgo-jsf-PrimeFaces/letsgo-jsf-PrimeFaces-xhtml/letsgo-jsf-PrimeFaces-xhtml-degree/src/main/java/org/igo/letsgo/jsf/degree/primefaces/DegreeManagedBean.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2016 pl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.igo.letsgo.jsf.degree.primefaces;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author pl
+ */
+@ManagedBean(name="DegreeManagedBean")
+@SessionScoped
+public class DegreeManagedBean implements Serializable {
+
+    private String degreeName="Мастер вселенной";
+    private String pageHeader="Управление спортивными разрядами";
+
+    /**
+     * @return the degreeName
+     */
+    public String getDegreeName() {
+        return degreeName;
+    }
+
+    /**
+     * @param degreeName the degreeName to set
+     */
+    public void setDegreeName(String degreeName) {
+        this.degreeName = degreeName;
+    }
+
+    /**
+     * @return the pageHeader
+     */
+    public String getPageHeader() {
+        return pageHeader;
+    }
+
+    /**
+     * @param pageHeader the pageHeader to set
+     */
+    public void setPageHeader(String pageHeader) {
+        this.pageHeader = pageHeader;
+    }
+    
+}
