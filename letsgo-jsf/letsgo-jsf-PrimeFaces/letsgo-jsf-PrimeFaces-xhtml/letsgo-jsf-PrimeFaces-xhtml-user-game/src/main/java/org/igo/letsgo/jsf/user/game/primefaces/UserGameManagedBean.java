42df364b4c6c14bc52dac901cf56d84ee3464c2a
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2016-10-12 13:30:24 +0300

    I have added west menu to game PrimeFaces project.

---
+++ letsgo-jsf/letsgo-jsf-PrimeFaces/letsgo-jsf-PrimeFaces-xhtml/letsgo-jsf-PrimeFaces-xhtml-user-game/src/main/java/org/igo/letsgo/jsf/user/game/primefaces/UserGameManagedBean.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 pl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.igo.letsgo.jsf.user.game.primefaces;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author pl
+ */
+@SessionScoped
+@ManagedBean(name="UserGameManagedBean")
+public class UserGameManagedBean implements Serializable {
+
+    private String userGameName="Список игр";
+    private String pageHeader="Управление списком игр";
+
+    /**
+     * @return the userGameName
+     */
+    public String getUserGameName() {
+        return userGameName;
+    }
+
+    /**
+     * @param userGameName the userGameName to set
+     */
+    public void setUserGameName(String userGameName) {
+        this.userGameName = userGameName;
+    }
+
+    /**
+     * @return the pageHeader
+     */
+    public String getPageHeader() {
+        return pageHeader;
+    }
+
+    /**
+     * @param pageHeader the pageHeader to set
+     */
+    public void setPageHeader(String pageHeader) {
+        this.pageHeader = pageHeader;
+    }
+}
