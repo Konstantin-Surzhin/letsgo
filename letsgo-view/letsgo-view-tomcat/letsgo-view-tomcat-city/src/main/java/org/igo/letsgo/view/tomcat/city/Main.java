d578a0b27c99eb7e3117420c84933f985aba90b8
surzhin.konstantin <surzhin.konstantin@L403LABSURZHIN.vniizht.lan>
2017-01-20 10:27:44 +0300

    I have added embedded tomcat city project

---
+++ letsgo-view/letsgo-view-tomcat/letsgo-view-tomcat-city/src/main/java/org/igo/letsgo/view/tomcat/city/Main.java
@@ -0,0 +1,67 @@
+package org.igo.letsgo.view.tomcat.city;
+
+/*
+ * Copyright (C) 2017 surzhin.konstantin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.io.File;
+import javax.servlet.ServletException;
+import org.apache.catalina.LifecycleException;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+/**
+ *
+ * @author surzhin.konstantin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     * @throws javax.servlet.ServletException
+     * @throws org.apache.catalina.LifecycleException
+     */
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        final String webappDirLocation = "src/main/webapp/";
+        final Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+    
+}
