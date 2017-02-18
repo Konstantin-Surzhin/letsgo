3cc40e8028327a23e8e55370767c0d86f075acf9
pl <pl@pl-Lenovo-Z580>
2017-02-18 13:25:19 +0300

    I have added ContiPerf benchmark test for City entity.

---
+++ letsgo-data/letsgo-entity/src/test/java/org/igo/contiperf/CityMBTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017 pl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.igo.contiperf;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+
+/**
+ *
+ * @author pl
+ */
+public class CityMBTest {
+
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
+
+    public CityMBTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    @PerfTest(invocations = 1, threads = 1)
+    @Required(max = 1200, average = 250)
+    public void hello() throws InterruptedException {
+        Thread.sleep(200);
+    }
+}
