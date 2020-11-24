785ff923976c215772b3e97854514faa60f41ac9
pl <pl@pl-Lenovo-G780>
2020-11-24 09:57:12 +0300

    Добавил проект с модулями spring-boot

---
+++ spring-boot/src/main/java/org/letsgo/letsgo/LetsgoApplication.java
@@ -0,0 +1,13 @@
+package org.letsgo.letsgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsgoApplication.class, args);
+	}
+
+}
