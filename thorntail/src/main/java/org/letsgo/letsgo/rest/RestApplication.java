94f6f9f2190dc76f20fb22ebdd185bd4f3f70fe1
pl <pl@pl-Lenovo-G780>
2020-11-24 09:29:35 +0300

    Добавил проект с сервером thorntail

---
+++ thorntail/src/main/java/org/letsgo/letsgo/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package org.letsgo.letsgo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
